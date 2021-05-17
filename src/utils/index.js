3df26047161d55a937a2759dfbe80deded2fff84
DESKTOP-JVIJUL5\55659 <mayaramaaraujo98@gmail.com>
2021-05-17 19:54:14 -0300

    deletion of the api key

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,5 @@
 export const BASE_URL = "https://www.googleapis.com/books/v1";
-export const API_KEY = "AIzaSyC6woOYauTdeCLLQCfHaUmWaiJ1WAhUQ28";
+export const API_KEY = "";
 
 export const goToPages = (history, route) => {
   history.push(route)
